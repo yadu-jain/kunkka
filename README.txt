2c22aaf138d764b1f7264aa9ffebcbc9a42eeb60
heera <heera.jaiswal@travelyaari.com>
2013-10-28 18:59:47 +0530

    Created kunkka

---
+++ README.txt
@@ -0,0 +1,14 @@
+kunkka README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/initialize_kunkka_db development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='kunkka',
+      version='0.0',
+      description='kunkka',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='kunkka',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = kunkka:main
+      [console_scripts]
+      initialize_kunkka_db = kunkka.scripts.initializedb:main
+      """,
+      )
